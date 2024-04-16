b7c96a6659f5356dcbd8c31a5ca95ec106f6491a
Rodrigo Moreira <rodrigomoreir4@gmail.com>
2024-04-16 19:25:41 -0300

    Projeto mscartoes criado

---
+++ mscartoes/src/test/java/com/rodrigomoreira/mscartoes/MscartoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigomoreira.mscartoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscartoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
