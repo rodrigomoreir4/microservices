64a41cbf536060979b1c0c1ad4f7f332a1a88bf0
Rodrigo Moreira <rodrigomoreir4@gmail.com>
2024-04-22 15:08:29 -0300

    Projeto msavaliadorcredito criado

---
+++ msavaliadorcredito/src/main/java/com/rodrigomoreira/msavaliadorcredito/MsavaliadorcreditoApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigomoreira.msavaliadorcredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsavaliadorcreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsavaliadorcreditoApplication.class, args);
+	}
+
+}
