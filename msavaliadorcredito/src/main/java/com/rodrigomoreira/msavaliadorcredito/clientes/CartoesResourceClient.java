ce85ef8758fa4ab697cb2eddd5217bea9bf65576
Rodrigo Moreira <rodrigomoreir4@gmail.com>
2024-04-22 19:51:29 -0300

    Buscando os dados de cartoes pelo microservice de cartoes

---
+++ msavaliadorcredito/src/main/java/com/rodrigomoreira/msavaliadorcredito/clientes/CartoesResourceClient.java
@@ -0,0 +1,18 @@
+package com.rodrigomoreira.msavaliadorcredito.clientes;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.rodrigomoreira.msavaliadorcredito.domain.CartaoCliente;
+
+@FeignClient(value = "mscartoes", path = "/cartoes")
+public interface CartoesResourceClient {
+    
+    @GetMapping(params = "cpf")
+    ResponseEntity<List<CartaoCliente>> getCartoesByCliente(@RequestParam("cpf") String cpf);
+
+}
--- msavaliadorcredito/src/main/java/com/rodrigomoreira/msavaliadorcredito/services/AvaliadorCreditoService.java
+++ msavaliadorcredito/src/main/java/com/rodrigomoreira/msavaliadorcredito/services/AvaliadorCreditoService.java
@@ -1,9 +1,13 @@
 package com.rodrigomoreira.msavaliadorcredito.services;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.rodrigomoreira.msavaliadorcredito.clientes.CartoesResourceClient;
 import com.rodrigomoreira.msavaliadorcredito.clientes.ClienteResourceClient;
+import com.rodrigomoreira.msavaliadorcredito.domain.CartaoCliente;
 import com.rodrigomoreira.msavaliadorcredito.domain.DadosCliente;
 import com.rodrigomoreira.msavaliadorcredito.domain.SituacaoCliente;
 
@@ -11,18 +15,22 @@ import com.rodrigomoreira.msavaliadorcredito.domain.SituacaoCliente;
 public class AvaliadorCreditoService {
     
     private ClienteResourceClient clientesClient;
+    private CartoesResourceClient cartoesClient;
 
-    public AvaliadorCreditoService(ClienteResourceClient clientesClient) {
+    public AvaliadorCreditoService(ClienteResourceClient clientesClient, CartoesResourceClient cartoesClient) {
         this.clientesClient = clientesClient;
+        this.cartoesClient = cartoesClient;
     }
 
     public SituacaoCliente obterSituacaoCliente(String cpf){
 
         ResponseEntity<DadosCliente> dadosClienteResponse = clientesClient.dadosCliente(cpf);
+        ResponseEntity<List<CartaoCliente>> cartoesResponse = cartoesClient.getCartoesByCliente(cpf);
 
         return SituacaoCliente
                 .builder()
                 .cliente(dadosClienteResponse.getBody())
+                .cartoes(cartoesResponse.getBody())
                 .build();
     }
 }
